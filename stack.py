4bca098e2e4d1b8b04e58c8efdbb3d27a69ade93
Fabio Rodriguez <fabiorodriguez444@gmail.com>
2024-05-02 19:11:46 -0300

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,32 @@
+# Stack:
+# This data structure acts as the opposite of queues, as the last element imputed
+# is the first to go out (LIFO - last in first out).
+
+class Stack:
+    def __init__(self):
+        self.elements = []
+
+    def __str__(self):
+        return str(self.elements)
+
+    def push(self, value):
+        self.elements.append(value)
+
+    def pop(self):
+        self.elements.pop()
+
+    def peek(self):
+        return self.elements[len(self.elements) - 1]
+
+    def is_empty(self):
+        return self.elements == []
+
+
+# Testing:
+s = Stack()
+for idx in range(10):
+    s.push(idx)
+print(s.peek())  # 9
+s.pop()  # bye 9
+s.pop()  # bye 8
+print(s.peek())  # 7
